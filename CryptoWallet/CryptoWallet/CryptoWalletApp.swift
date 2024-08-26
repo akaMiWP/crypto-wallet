48df73f6c7766e5284c024bc21cbfa3e31befb32
akaMiWP <bangkokappdevelopment@gmail.com>
2024-08-26 19:08:48 +0700

    Init a project

---
+++ CryptoWallet/CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2567 BE akaMiWP. All rights reserved.
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
