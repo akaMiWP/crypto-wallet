c1039538e4fced4e264d99afeecc54a6add3eb93
akaMiWP <bangkokappdevelopment@gmail.com>
2024-08-31 19:20:50 +0700

    Implemented TopBarView and TokensListView

--- CryptoWallet/CryptoWallet/Dashboard/DashboardView.swift
+++ CryptoWallet/CryptoWallet/Dashboard/DashboardView.swift
@@ -4,38 +4,53 @@ import SwiftUI
 
 struct DashboardView: View {
     var body: some View {
-        makeNetworkSwitchView()
+        makeTopBarView()
         
-        Text("$21.10")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-        
-        makeOperationViews()
-        
-        Spacer()
+        ScrollView {
+            Text("$21.10")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            makeOperationViews()
+            
+            makeERC20TokensListView()
+            
+            Spacer()
+        }
     }
 }
 
 // MARK: - Private
 private extension DashboardView {
-    func makeNetworkSwitchView() -> some View {
-        HStack {
-            Text("S")
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding(12)
-                .background(Color.blue)
-                .clipShape(Circle())
+    func makeTopBarView() -> some View {
+        ZStack {
             
-            Text("Network")
-                .font(.subheadline)
+            HStack {
+                Text("Account 1")
+                    .font(.headline)
+                
+                Image(systemName: "chevron.down")
+            }
             
-            Image(systemName: "chevron.down")
+            HStack {
+                HStack {
+                    Text("S")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding(8)
+                        .background(Color.blue)
+                        .clipShape(Circle())
+                    
+                    Image(systemName: "chevron.down")
+                }
+                .padding(.trailing, 12)
+                .background(Color.blue.opacity(0.2))
+                .clipShape(RoundedRectangle(cornerSize: .init(width: 32, height: 32)))
+                
+                Spacer()
+            }
         }
-        .padding(.trailing, 12)
-        .background(Color.blue.opacity(0.2))
-        .clipShape(RoundedRectangle(cornerSize: .init(width: 32, height: 32)))
-        .padding()
+        .padding(.horizontal)
     }
     
     func makeOperationViews() -> some View {
@@ -58,6 +73,39 @@ private extension DashboardView {
         .frame(width: 100, height: 80)
         .background(Color.blue.opacity(0.2))
         .clipShape(RoundedRectangle(cornerSize: .init(width: 16, height: 16)))
+    }
+    
+    func makeERC20TokensListView() -> some View {
+        ForEach(0..<12) { _ in
+            makeERC20TokenView()
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(Color.blue.opacity(0.1))
+        .clipShape(RoundedRectangle(cornerSize: .init(width: 16, height: 16)))
+        .padding(.horizontal)
+    }
+    
+    func makeERC20TokenView() -> some View {
+        HStack(alignment: .top) {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading) {
+                Text("Token Name")
+                    .font(.headline)
+                
+                Text("0.123456 ETH")
+                    .font(.subheadline)
+            }
+            
+            Spacer()
+            
+            Text("$1234")
+                .font(.headline)
+        }
     }
 }
 
