c72f4225122f9eeb41516fc2bbbf48dcdcec1a4c
akaMiWP <bangkokappdevelopment@gmail.com>
2024-09-07 14:10:30 +0700

    Added AlertViewModel as a generic alert model so that it could be used across the app

---
+++ CryptoWallet/CryptoWallet/Models/Presentation/AlertViewModel.swift
@@ -0,0 +1,18 @@
+// Copyright © 2567 BE akaMiWP. All rights reserved.
+
+import Foundation
+
+struct AlertViewModel {
+    let title: String
+    let message: String
+    let dismissAction: (() -> Void)?
+    
+    init(title: String = "Error",
+         message: String = "Something went wrong !, please try again",
+         dismissAction: (() -> Void)? = nil
+    ) {
+        self.title = title
+        self.message = message
+        self.dismissAction = dismissAction
+    }
+}
