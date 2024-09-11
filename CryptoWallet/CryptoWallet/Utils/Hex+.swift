4766839ebc8a55f5590e8d9e8304921b71673450
akaMiWP <bangkokappdevelopment@gmail.com>
2024-09-12 04:51:56 +0700

    Added Hex+ to convert hex into double

--- CryptoWallet/CryptoWallet/Utils/Hex+.swift
+++ CryptoWallet/CryptoWallet/Utils/Hex+.swift
@@ -1,3 +1,12 @@
 // Copyright © 2567 BE akaMiWP. All rights reserved.
 
+import BigInt
 import Foundation
+
+func convertHexToDouble(hexString: String, decimals: Int = 18) -> Double? {
+    let hexValue = hexString.replacingOccurrences(of: "0x", with: "")
+    guard let bigUIntValue = BigUInt(hexValue, radix: 16) else { return nil }
+    let decimalFactor = pow(10, Double(decimals))
+    let balanceInDouble = Double(bigUIntValue) / decimalFactor
+    return balanceInDouble
+}
