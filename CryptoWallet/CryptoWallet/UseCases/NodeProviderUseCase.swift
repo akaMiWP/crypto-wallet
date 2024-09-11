b34cd65714e1219cdd5ef286d71c32935b31fdf3
akaMiWP <bangkokappdevelopment@gmail.com>
2024-09-12 04:52:09 +0700

    Added NodeProviderUseCase

--- CryptoWallet/CryptoWallet/UseCases/NodeProviderUseCase.swift
+++ CryptoWallet/CryptoWallet/UseCases/NodeProviderUseCase.swift
@@ -1,3 +1,32 @@
 // Copyright © 2567 BE akaMiWP. All rights reserved.
 
+import Combine
 import Foundation
+
+protocol NodeProviderUseCase {
+    func fetchTokenBalances(address: String) -> AnyPublisher<[AddressToTokenModel], Error>
+}
+
+final class NodeProviderImpl: NodeProviderUseCase {
+    
+    private let networkStack: NetworkStack
+    
+    init(networkStack: NetworkStack) {
+        self.networkStack = networkStack
+    }
+    
+    func fetchTokenBalances(address: String) -> AnyPublisher<[AddressToTokenModel], Error> {
+        let tokenBalancesResponsePublisher: AnyPublisher<JSONRPCResponse<TokenBalancesResponse>, Error> = networkStack
+            .fetchServiceProviderAPI(method: .tokenBalances, params: [address])
+        
+        let modelsPublisher = tokenBalancesResponsePublisher.map { response in
+            let models: [AddressToTokenModel] = response.result.tokenBalances.compactMap {
+                guard let tokenBalance = convertHexToDouble(hexString: $0.tokenBalance) else { return nil }
+                return .init(address: $0.contractAddress, tokenBalance: tokenBalance)
+            }
+            return models
+        }
+        
+        return modelsPublisher.eraseToAnyPublisher()
+    }
+}
