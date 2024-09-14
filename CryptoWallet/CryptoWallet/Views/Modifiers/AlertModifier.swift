87c1d0cc758b53a553d207bf07867a6df6e0caf3
akaMiWP <bangkokappdevelopment@gmail.com>
2024-09-14 15:59:06 +0700

    Working on Alertable and AlertViewModifier

---
+++ CryptoWallet/CryptoWallet/Views/Modifiers/AlertModifier.swift
@@ -0,0 +1,39 @@
+// Copyright © 2567 BE akaMiWP. All rights reserved.
+
+import SwiftUI
+
+protocol Alertable: ObservableObject {
+    var showAlert: Bool { get set }
+    var alertViewModel: AlertViewModel? { get set }
+}
+
+struct AlertModifier<ViewModel: Alertable>: ViewModifier {
+    @ObservedObject var viewModel: ViewModel
+    
+    func body(content: Content) -> some View {
+        content
+            .alert(
+            viewModel.alertViewModel?.title ?? "",
+            isPresented: isPresented,
+            presenting: viewModel.alertViewModel,
+            actions: { _ in },
+            message: { viewModel in
+                Text(viewModel.message)
+            }
+        )
+    }
+}
+
+//MARK: - Private
+private extension AlertModifier {
+    var isPresented: Binding<Bool> {
+        .init(
+            get: { viewModel.showAlert},
+            set: { newValue in
+                if !newValue {
+                    viewModel.showAlert = false
+                }
+            }
+        )
+    }
+}
