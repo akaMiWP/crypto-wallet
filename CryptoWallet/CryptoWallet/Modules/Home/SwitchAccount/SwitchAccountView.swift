506f3c3dcb9fa832c1c8eeec77cc23aeb1f9dda6
akaMiWP <bangkokappdevelopment@gmail.com>
2024-09-23 15:23:55 +0700

    Styled SwitchAccountView

--- CryptoWallet/CryptoWallet/Modules/Home/SwitchAccount/SwitchAccountView.swift
+++ CryptoWallet/CryptoWallet/Modules/Home/SwitchAccount/SwitchAccountView.swift
@@ -7,28 +7,29 @@ struct SwitchAccountView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack(spacing: 16) {
+        VStack(alignment: .leading, spacing: 16) {
             Text("Select an account")
-                .font(.title)
+                .font(.title2)
                 .fontWeight(.bold)
+                .padding(.top)
             
             ForEach(viewModel.wallets, id: \.address) { wallet in
                 HStack {
                     VStack(alignment: .leading) {
                         Text(wallet.name)
                             .fontWeight(.bold)
-                        Text(wallet.address)
+                            .font(.subheadline)
+                        Text(wallet.maskedAddress)
+                            .font(.subheadline)
                     }
-                    .font(.subheadline)
                     
                     Spacer()
                 }
                 .frame(maxWidth: .infinity)
                 .frame(height: 50)
                 .padding(.horizontal)
-                .background(.blue)
+                .background(.blue.opacity(0.7))
                 .clipShape(RoundedRectangle(cornerSize: .init(width: 16, height: 16)))
-                .padding(.horizontal)
                 .onTapGesture {
                     viewModel.selectWallet(wallet: wallet)
                     dismiss()
@@ -43,13 +44,26 @@ struct SwitchAccountView: View {
                 dismiss()
             }, label: {
                 Text("Create a new wallet")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .font(.subheadline)
             })
+            .frame(height: 46)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerSize: .init(width: 16, height: 16)))
         }
+        .padding(.horizontal)
         .modifier(AlertModifier(viewModel: viewModel))
         .onAppear { viewModel.loadWallets() }
     }
 }
 
 #Preview {
-    SwitchAccountView(viewModel: .init(manageWalletsUseCase: ManageWalletsImpl()))
+    HDWalletManager.shared.createdWalletModels = [
+        .init(name: "Account #1", address: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045"),
+        .init(name: "Account #2", address: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045"),
+        .init(name: "Account #3", address: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045")
+    ]
+    return SwitchAccountView(viewModel: .init(manageWalletsUseCase: ManageWalletsImpl()))
 }
--- CryptoWallet/CryptoWallet/Utils/String+.swift
+++ CryptoWallet/CryptoWallet/Utils/String+.swift
@@ -2,8 +2,8 @@
 
 extension String {
     func maskedWalletAddress() -> String {
-        let frontRange = startIndex..<index(startIndex, offsetBy: 5)
-        let backRange = index(endIndex, offsetBy: -4)..<endIndex
+        let frontRange = startIndex..<index(startIndex, offsetBy: 7)
+        let backRange = index(endIndex, offsetBy: -5)..<endIndex
         return self[frontRange] + "...." + self[backRange]
     }
 }
