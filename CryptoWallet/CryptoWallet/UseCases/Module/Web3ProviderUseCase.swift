9102cd34cbc773acaec5dc2eb11f5685ea959e7c
akaMiWP <bangkokappdevelopment@gmail.com>
2024-10-14 14:13:58 +0700

    Removed web3swift and added web3.swift

---
+++ CryptoWallet/CryptoWallet/UseCases/Module/Web3ProviderUseCase.swift
@@ -0,0 +1,12 @@
+// Copyright © 2567 BE akaMiWP. All rights reserved.
+
+import web3swift
+
+protocol Web3ProviderUseCase {}
+
+final class Web3ProviderImp: Web3ProviderUseCase {
+    
+    init() {
+        
+    }
+}
