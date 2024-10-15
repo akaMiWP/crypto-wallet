c8ca6e256b984a14218d44ae549b35cdfea53f9a
akaMiWP <bangkokappdevelopment@gmail.com>
2024-10-15 20:07:38 +0700

    Added ReceiptViewState

--- CryptoWallet/CryptoWallet/Modules/Home/Summary/TransactionReceiptView.swift
+++ CryptoWallet/CryptoWallet/Modules/Home/Summary/TransactionReceiptView.swift
@@ -2,33 +2,25 @@
 
 import SwiftUI
 
-struct TransactionReceiptView: View {
-    var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Text("Transaction Pending")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.secondaryGreen1_900)
-                
-                Spacer()
-                
-                Image(systemName: "xmark")
-                    .foregroundColor(Color.secondaryGreen1_900)
-            }
-            .padding(.horizontal, 24)
-            
-            buildDetailRow(isLoading: false, title: "Initiate Transaction")
-            buildDetailRow(isLoading: true, title: "Processing Transaction", subtitle: "This may take a few minutes")
-        }
-        .frame(maxWidth: .infinity)
-        .padding(.vertical)
-        .background(Color.secondaryGreen1_100)
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-        .padding()
-    }
+enum ReceiptViewState: Equatable {
+    case initiatingTransaction
+    case processingTransaction
+    case confirmedTransaction(txHash: String)
 }
 
+struct TransactionReceiptView: View {
+    
+    @Binding var viewState: ReceiptViewState
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.4).ignoresSafeArea()
+            
+            buildDetailRows()
+                .animation(.easeIn, value: viewState)
+        }
+    }
+}
 private extension TransactionReceiptView {
     func buildDetailRow(
         isLoading: Bool,
@@ -63,8 +55,82 @@ private extension TransactionReceiptView {
         .padding(.horizontal, 24)
         .padding(.vertical)
     }
+    
+    func buildDetailRows() -> some View {
+        switch viewState {
+        case .initiatingTransaction:
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Transaction Pending")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.secondaryGreen1_900)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color.secondaryGreen1_900)
+                }
+                .padding(.horizontal, 24)
+                
+                buildDetailRow(isLoading: true, title: "Initiate Transaction")
+                buildDetailRow(isLoading: true, title: "Processing Transaction")
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical)
+            .background(Color.secondaryGreen1_100)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .padding()
+        case .processingTransaction:
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Transaction Pending")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.secondaryGreen1_900)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color.secondaryGreen1_900)
+                }
+                .padding(.horizontal, 24)
+                
+                buildDetailRow(isLoading: false, title: "Initiate Transaction")
+                buildDetailRow(isLoading: true, title: "Processing Transaction", subtitle: "This may take a few minutes")
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical)
+            .background(Color.secondaryGreen1_100)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .padding()
+        case .confirmedTransaction(let txHash):
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Transaction Pending")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.secondaryGreen1_900)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color.secondaryGreen1_900)
+                }
+                .padding(.horizontal, 24)
+                
+                buildDetailRow(isLoading: false, title: "Initiate Transaction")
+                buildDetailRow(isLoading: false, title: "Processing Transaction", subtitle: "\(txHash)")
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical)
+            .background(Color.secondaryGreen1_100)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .padding()
+        }
+    }
 }
 
 #Preview {
-    TransactionReceiptView()
+    TransactionReceiptView(viewState: .constant(.initiatingTransaction))
 }
