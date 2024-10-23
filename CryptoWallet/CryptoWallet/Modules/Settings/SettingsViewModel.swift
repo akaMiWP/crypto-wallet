fadb803acb1cbe6ab1a427a6c9b85529ed3d3a7f
akaMiWP <bangkokappdevelopment@gmail.com>
2024-10-23 19:39:05 +0700

    Implemented presentation logic when tap on reveal seed phrase

--- CryptoWallet/CryptoWallet/Modules/Settings/SettingsViewModel.swift
+++ CryptoWallet/CryptoWallet/Modules/Settings/SettingsViewModel.swift
@@ -2,15 +2,18 @@
 
 import Combine
 
-final class SettingsViewModel: ObservableObject {
-    
+final class SettingsViewModel: Alertable {
     let rows: [SettingsRowViewModel] = [
         .init(rowType: .changeTheme),
         .init(rowType: .revealSeedPhrase),
         .init(rowType: .resetWallet)
     ]
     
+    @Published var mneumonic: String?
+    @Published var alertViewModel: AlertViewModel?
+    
     private let manageHDWalletUseCase: ManageHDWalletUseCase
+    private var cancellables: Set<AnyCancellable> = .init()
     
     init(manageHDWalletUseCase: ManageHDWalletUseCase) {
         self.manageHDWalletUseCase = manageHDWalletUseCase
@@ -21,6 +24,22 @@ final class SettingsViewModel: ObservableObject {
     func didTapChangeTheme() {}
     
     func didTapRevealSeedPhrase() {
-        manageHDWalletUseCase.retrieveMneumonic()
+        manageHDWalletUseCase
+            .retrieveMneumonicPublisher()
+            .sink { [weak self] in
+                if case .failure(let error) = $0 {
+                    self?.handleError(error)
+                }
+            } receiveValue: { [weak self] in
+                self?.mneumonic = $0
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - Private
+private extension SettingsViewModel {
+    func handleError(_ error: Error) {
+        alertViewModel = .init()
     }
 }
